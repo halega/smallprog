ccb1e88acde8b5816db5f2c947949d27878ab4a5
Stanislav Kalashnikov <stas@halega.com>
2018-12-04 09:59:42 +0300

    removeeven: init module

--- removeeven/removeeven.go
+++ removeeven/removeeven.go
@@ -1,6 +1,6 @@
 package removeeven
 
-// Remove removes even numbers from slice. It reuses underlying array.
+// Remove removes even numbers from a slice. It reuses underlying array.
 func Remove(a []int) []int {
 	odd := a[:0]
 	for _, n := range a {
